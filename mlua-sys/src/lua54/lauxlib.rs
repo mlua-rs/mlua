514ec24252d438e04632f1de6ad47be80e614ebb
Alex Orlenko <zxteam@protonmail.com>
2024-01-03 12:02:32 +0000

    Fix lua53/lua54 `luaL_error` definition (for wasm32)

--- mlua-sys/src/lua54/lauxlib.rs
+++ mlua-sys/src/lua54/lauxlib.rs
@@ -50,7 +50,7 @@ extern "C-unwind" {
     pub fn luaL_checkudata(L: *mut lua_State, ud: c_int, tname: *const c_char) -> *mut c_void;
 
     pub fn luaL_where(L: *mut lua_State, lvl: c_int);
-    pub fn luaL_error(L: *mut lua_State, fmt: *const c_char, ...) -> !;
+    pub fn luaL_error(L: *mut lua_State, fmt: *const c_char, ...) -> c_int;
 
     pub fn luaL_checkoption(
         L: *mut lua_State,
