71a039939c7b81b6982f215e971c246227b4d143
joshw <joshw@fedora.tailc33ba.ts.net>
2024-07-13 12:35:31 -0500

    updated to use userdata instead of table

--- mlua_derive/src/to_lua.rs
+++ mlua_derive/src/to_lua.rs
@@ -1,6 +1,9 @@
 use proc_macro::TokenStream;
 use quote::{quote, format_ident};
-use syn::{parse_macro_input, DeriveInput, Data, Fields};
+use syn::{parse_macro_input, DeriveInput, Data, Fields, Type};
+use syn::spanned::Spanned;
+use syn::punctuated::Punctuated;
+use proc_macro2::TokenStream as TokenStream2;
 
 pub fn to_lua(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -15,23 +18,60 @@ pub fn to_lua(input: TokenStream) -> TokenStream {
         panic!("ToLua can only be derived for structs");
     };
 
-    let set_fields = fields.named.iter().map(|field| {
+    let add_field_methods = fields.named.iter().map(|field| {
         let name = &field.ident;
         let name_str = name.as_ref().unwrap().to_string();
+        let ty = &field.ty;
+
+        let get_method = if is_copy_type(ty) {
+            quote! {
+                fields.add_field_method_get(#name_str, |_, this| Ok(this.#name));
+            }
+        } else {
+            quote! {
+                fields.add_field_method_get(#name_str, |_, this| Ok(this.#name.clone()));
+            }
+        };
+
+        let set_method = quote! {
+            fields.add_field_method_set(#name_str, |_, this, val| {
+                this.#name = val;
+                Ok(())
+            });
+        };
+
         quote! {
-            table.set(#name_str, self.#name)?;
+            #get_method
+            #set_method
         }
     });
 
     let gen = quote! {
-        impl<'lua> ::mlua::IntoLua<'lua> for #ident {
-            fn into_lua(self, lua: &'lua ::mlua::Lua) -> ::mlua::Result<::mlua::Value<'lua>> {
-                let table = lua.create_table()?;
-                #(#set_fields)*
-                Ok(::mlua::Value::Table(table))
+        impl ::mlua::UserData for #ident {
+            fn add_fields<'lua, F: ::mlua::prelude::LuaUserDataFields<'lua, Self>>(fields: &mut F) {
+                #(#add_field_methods)*
             }
         }
     };
 
     gen.into()
+}
+
+// I don't know how to determine whether or not something implements copy, so for now everything
+// will be cloned that isn't one of these copyable primitives.
+fn is_copy_type(ty: &Type) -> bool {
+    match ty {
+        Type::Path(type_path) => {
+            let segments = &type_path.path.segments;
+            let segment = segments.last().unwrap();
+            match segment.ident.to_string().as_str() {
+                "u8" | "u16" | "u32" | "u64" | "u128" |
+                "i8" | "i16" | "i32" | "i64" | "i128" |
+                "f32" | "f64" |
+                "bool" | "char" | "usize" | "isize" => true,
+                _ => false,
+            }
+        }
+        _ => false,
+    }
 }
