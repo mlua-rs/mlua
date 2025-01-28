47bc372096f942349ff2cf781d6f7c377b275e06
tk <49250442+tkr-sh@users.noreply.github.com>
2025-01-28 10:10:28 +0000

    `impl FromLua/IntoLua` for `char` (#516)

--- src/conversion.rs
+++ src/conversion.rs
@@ -655,6 +655,71 @@ impl IntoLua for &Path {
     }
 }
 
+impl IntoLua for char {
+    #[inline]
+    fn into_lua(self, lua: &Lua) -> Result<Value> {
+        let mut char_bytes = [0; 4];
+        self.encode_utf8(&mut char_bytes);
+        Ok(Value::String(lua.create_string(char_bytes)?))
+    }
+}
+
+impl FromLua for char {
+    #[inline]
+    fn from_lua(value: Value, _lua: &Lua) -> Result<Self> {
+        let ty = value.type_name();
+        match value {
+            // When integer: reduce it to u8 and try to convert to char
+            Value::Integer(i) => {
+                if i <= u8::MAX.into() && i >= 0 {
+                    Ok(char::from(i as u8))
+                } else {
+                    Err(Error::FromLuaConversionError {
+                        from: ty,
+                        to: Self::type_name(),
+                        message: Some(
+                            "expected int to be a u8 when converting to char. You can also use strings."
+                                .to_string(),
+                        ),
+                    })
+                }
+            }
+            // When String: first char, and only if there is one char
+            Value::String(s) => {
+                let str = s.to_str()?;
+                let mut str_iter = str.chars();
+                let Some(char) = str_iter.next() else {
+                    return Err(Error::FromLuaConversionError {
+                        from: ty,
+                        to: Self::type_name(),
+                        message: Some(
+                            "string must have one char when converting to char. (empty string)".to_string(),
+                        ),
+                    });
+                };
+
+                if let Some(_extra_char) = str_iter.next() {
+                    Err(Error::FromLuaConversionError {
+                        from: ty,
+                        to: Self::type_name(),
+                        message: Some(
+                            "expected lua string to have exactly one char when converting to char"
+                                .to_string(),
+                        ),
+                    })
+                } else {
+                    Ok(char)
+                }
+            }
+            _ => Err(Error::FromLuaConversionError {
+                from: ty,
+                to: Self::type_name(),
+                message: Some("expected string or integer".to_string()),
+            }),
+        }
+    }
+}
+
 #[inline]
 unsafe fn push_bytes_into_stack<T>(this: T, lua: &RawLua) -> Result<()>
 where
