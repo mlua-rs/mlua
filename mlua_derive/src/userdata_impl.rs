023e4c61d843f10ff4409145f0809ed8f51dafbd
Alex Orlenko <zxteam@protonmail.com>
2026-05-18 22:44:00 +0100

    mlua_derive: Fix compilation / remove "if let" guards

--- mlua_derive/src/userdata_impl.rs
+++ mlua_derive/src/userdata_impl.rs
@@ -71,8 +71,12 @@ fn classify_ref_type(ty: &Type) -> Option<Type> {
     if ref_ty.mutability.is_none() {
         let lookup_name: Option<String> = match &*ref_ty.elem {
             Type::Path(path) => path.path.segments.last().map(|seg| seg.ident.to_string()),
-            Type::Slice(slice) if let Type::Path(path) = &*slice.elem => {
-                path.path.segments.last().map(|seg| format!("[{}]", seg.ident))
+            Type::Slice(slice) => {
+                if let Type::Path(path) = &*slice.elem {
+                    path.path.segments.last().map(|seg| format!("[{}]", seg.ident))
+                } else {
+                    None
+                }
             }
             _ => None,
         };
