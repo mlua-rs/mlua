208a70f4077b78b707dfde48b7a8cc80c25f1129
Alex Orlenko <zxteam@protonmail.com>
2026-05-31 11:17:51 +0100

    Move inlined `#[lua_module]` proc macro doc to docs/lua_module.md

---
+++ docs/lua_module.md
@@ -0,0 +1,41 @@
+Registers Lua module entrypoint.
+
+You can register multiple entrypoints as required.
+
+```ignore
+use mlua::{Lua, Result, Table};
+
+#[mlua::lua_module]
+fn my_module(lua: &Lua) -> Result<Table> {
+    let exports = lua.create_table()?;
+    exports.set("hello", "world")?;
+    Ok(exports)
+}
+```
+
+Internally in the code above the compiler defines C function `luaopen_my_module`.
+
+You can also pass options to the attribute:
+
+* name - name of the module, defaults to the name of the function
+
+```ignore
+#[mlua::lua_module(name = "alt_module")]
+fn my_module(lua: &Lua) -> Result<Table> {
+    ...
+}
+```
+
+* skip_memory_check - skip memory allocation checks for some operations.
+
+In module mode, mlua runs in an unknown environment and cannot tell whether there are any memory
+limits or not. As a result, some operations that require memory allocation run in protected
+mode. Setting this attribute will improve performance of such operations with risk of having
+uncaught exceptions and memory leaks.
+
+```ignore
+#[mlua::lua_module(skip_memory_check)]
+fn my_module(lua: &Lua) -> Result<Table> {
+    ...
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -204,47 +204,7 @@ pub use mlua_derive::UserData;
 #[cfg_attr(docsrs, doc(cfg(feature = "macros")))]
 pub use mlua_derive::userdata_impl;
 
-/// Registers Lua module entrypoint.
-///
-/// You can register multiple entrypoints as required.
-///
-/// ```ignore
-/// use mlua::{Lua, Result, Table};
-///
-/// #[mlua::lua_module]
-/// fn my_module(lua: &Lua) -> Result<Table> {
-///     let exports = lua.create_table()?;
-///     exports.set("hello", "world")?;
-///     Ok(exports)
-/// }
-/// ```
-///
-/// Internally in the code above the compiler defines C function `luaopen_my_module`.
-///
-/// You can also pass options to the attribute:
-///
-/// * name - name of the module, defaults to the name of the function
-///
-/// ```ignore
-/// #[mlua::lua_module(name = "alt_module")]
-/// fn my_module(lua: &Lua) -> Result<Table> {
-///     ...
-/// }
-/// ```
-///
-/// * skip_memory_check - skip memory allocation checks for some operations.
-///
-/// In module mode, mlua runs in an unknown environment and cannot tell whether there are any memory
-/// limits or not. As a result, some operations that require memory allocation run in protected
-/// mode. Setting this attribute will improve performance of such operations with risk of having
-/// uncaught exceptions and memory leaks.
-///
-/// ```ignore
-/// #[mlua::lua_module(skip_memory_check)]
-/// fn my_module(lua: &Lua) -> Result<Table> {
-///     ...
-/// }
-/// ```
+#[doc = include_str!("../docs/lua_module.md")]
 #[cfg(all(feature = "mlua_derive", any(feature = "module", doc)))]
 #[cfg_attr(docsrs, doc(cfg(feature = "module")))]
 pub use mlua_derive::lua_module;
